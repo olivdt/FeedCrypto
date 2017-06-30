11be5d16836c3f75e32e10b6cf30cd0e56655a85
Olivier Viera <oliviervdt@gmail.com>
2017-06-30 20:55:39 +0400

    Refactoring. Merged Article and Feed.

--- rssParser/Feed.py
+++ rssParser/Feed.py
@@ -1,11 +1,14 @@
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from xml.etree.cElementTree import fromstring
+
+import newspaper
 from dateutil import parser
 import datetime
 from peewee import *
-from rssParser import Article, BaseModel
+from peewee import CharField, DateTimeField, ForeignKeyField
 
+from rssParser import Article, BaseModel
 
 class RssFeed(BaseModel.BaseModel):
     """ RSS feed main class
@@ -91,7 +94,7 @@ class RssFeed(BaseModel.BaseModel):
             pub_date = parser.parse(pub_date_str)
             link = item.findtext('link')
             if self._articles.__len__() == 0 or pub_date < self._articles[0].pub_date:
-                self._articles.append(Article.Article(title, link, pub_date))
+                self._articles.append(Article(title, link, pub_date))
                 self._new_articles_available = True
 
         self._articles.sort(key=lambda Article: Article.pub_date, reverse=True)
@@ -101,3 +104,67 @@ class RssFeed(BaseModel.BaseModel):
 
         self._last_download_date = datetime.datetime.today()
 
+
+class Article(BaseModel.BaseModel):
+    """ Article class that can be used with
+        any type of feed (RSS/Atom/
+    """
+
+    _title = CharField(primary_key=True)
+    _url = CharField(unique=True)
+    _pub_date = DateTimeField()
+    _text = CharField()
+    _rssFeed = ForeignKeyField(RssFeed, related_name='articles')
+
+    def __init__(self, title=None, url=None, pubDate=None):
+        super(Article, self).__init__()
+#        self._title = title if title != None else ''
+#        self._url = url if url != None else ''
+#        self._pub_date = pubDate if pubDate != None else ''
+
+        self._title = title
+        self._url = url
+        self._pub_date = pubDate
+
+
+        if self._title != None and self._url != None and self._pub_date != None:
+            self._articleParse = newspaper.Article(self.url)
+
+    @property
+    def url(self):
+        return self._url if hasattr(self, "_url") else 'No URL'
+
+    @property
+    def pub_date(self):
+        return self._pub_date if hasattr(self, "_pub_date") else 'No DATE'
+
+    @property
+    def authors(self):
+        return self._authors if hasattr(self, "_authors") else 'No AUTHORS'
+
+    @property
+    def text(self):
+        return self._text if hasattr(self, "_text") else 'No TEXT'
+
+    @property
+    def downloaded(self):
+        return self._downloaded if hasattr(self, "_downloaded") else ' '
+
+    @property
+    def title(self):
+        return self._title if hasattr(self, "_title") else ' '
+
+    def get_article_contents(self):
+        self._parse_article()
+
+    def _parse_article(self):
+        self._articleParse.download()
+        self._articleParse.parse()
+
+        if self._articleParse.is_parsed:
+            self._text = self._articleParse.text
+            self._authors = self._articleParse.authors
+            self._title = self._articleParse.title
+            self._downloaded = True
+
+        self.update_db()
